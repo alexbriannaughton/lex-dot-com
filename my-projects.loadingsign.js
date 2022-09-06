d6d3f762495fc806c6e4dcc7dee2bfc85efc04a4
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-09-05 22:47:04 -0700

    update

--- my-projects.loadingsign.js
+++ my-projects.loadingsign.js
@@ -3,7 +3,7 @@ button.addEventListener('click', function() {
   document.getElementById("loading").style.textDecoration = "none"
   document.getElementById("loading").style.fontStyle = "italic"
   const p = document.createElement("p");
-  p.textContent = `My software-engineering journey will officially begin in mid-September as I begin courses with Flatiron School; my projects are currently "loading" . . .`;
+  p.textContent = `Woops! No projects yet. My software-engineering journey will officially begin in mid-September as I begin courses with Flatiron School.`;
   p.className = "fade"
   document.body.append(p);
 }, { once: true} ); 
