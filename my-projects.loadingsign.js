49cc1af46418195f00f702f9a56790c103b52092
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-09-07 11:40:49 -0700

    update #me border and loading js blurb

--- my-projects.loadingsign.js
+++ my-projects.loadingsign.js
@@ -3,7 +3,7 @@ button.addEventListener('click', function() {
   document.getElementById("loading").style.textDecoration = "none"
   document.getElementById("loading").style.fontStyle = "italic"
   const p = document.createElement("p");
-  p.textContent = `Woops! No projects yet. My software-engineering journey will officially begin in mid-September as I begin courses with Flatiron School.`;
+  p.textContent = `Woops! No projects yet; they're still "loading..." My software-engineering journey will officially begin in mid-September as I begin courses with Flatiron School.`;
   p.className = "fade"
   document.body.append(p);
 }, { once: true} ); 
