85ce19147c387bcec329de69b83e6f006d243a7f
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-03-07 09:48:33 -0800

    video page iframes pause when another is played

--- stopIframe.js
+++ stopIframe.js
@@ -1,101 +1,99 @@
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-console.log(firstScriptTag)
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player1;
-var player2;
-var player3;
-var player4;
-var player5;
-function onYouTubeIframeAPIReady() {
-    player1 = new YT.Player('player1', {
-        height: '100%',
-        width: '100%',
-        videoId: 'ne1kx2UGRPs',
-        playerVars: {
-            'playsinline': 1,
-            'controls': 2
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-    player2 = new YT.Player('player2', {
-        height: '100%',
-        width: '100%',
-        videoId: '0MR-LYxXSvg',
-        playerVars: {
-            'playsinline': 1,
-            'controls': 2
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-    player3 = new YT.Player('player3', {
-        height: '100%',
-        width: '100%',
-        videoId: 'SPx3rZWjshM',
-        playerVars: {
-            'playsinline': 1,
-            'controls': 2
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-    player4 = new YT.Player('player4', {
-        height: '100%',
-        width: '100%',
-        videoId: 'uinlDrEq00g',
-        playerVars: {
-            'playsinline': 1,
-            'controls': 2
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-    player5 = new YT.Player('player5', {
-        height: '100%',
-        width: '100%',
-        videoId: 'Vzybd59aboU',
-        playerVars: {
-            'playsinline': 1,
-            'controls': 2
-        },
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-   
+var ytplayerList;
+
+function onPlayerReady(e) {
+    var video_data = e.target.getVideoData(),
+        label = video_data.video_id+':'+video_data.title;
+    e.target.ulabel = label;
+    // console.log(label + " is ready!");
+ 
 }
-
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.playVideo();
+function onPlayerError(e) {
+    console.log('[onPlayerError]');
 }
+function onPlayerStateChange(e) {
+    var label = e.target.ulabel;
+    if (e["data"] == YT.PlayerState.PLAYING) {
+        // console.log({
+        //     event: "youtube",
+        //     action: "play:"+e.target.getPlaybackQuality(),
+        //     label: label
+        // });
+        
+        //if one video is playing then pause other
+        pauseOthersYoutubes(e.target);
+    }
+    // if (e["data"] == YT.PlayerState.PAUSED) {
+    //     console.log({
+    //         event: "youtube",
+    //         action: "pause:"+e.target.getPlaybackQuality(),
+    //         label: label
+    //     });
+    // }
+    // if (e["data"] == YT.PlayerState.ENDED) {
+    //     console.log({
+    //         event: "youtube",
+    //         action: "end",
+    //         label: label
+    //     });
+    // }
+    //track number of buffering and quality of video
+    if (e["data"] == YT.PlayerState.BUFFERING) {
+        e.target.uBufferingCount?++e.target.uBufferingCount:e.target.uBufferingCount=1; 
+        // console.log({
+        //     event: "youtube",
+        //     action: "buffering["+e.target.uBufferingCount+"]:"+e.target.getPlaybackQuality(),
+        //     label: label
+        // });
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
+        //if one video is play then pause other, this is needed because at start video is in buffered state and start playing without go to playing state
+        if( YT.PlayerState.UNSTARTED ==  e.target.uLastPlayerState ){
+            pauseOthersYoutubes(e.target);
+        }
+    }
+    //last action keep stage in uLastPlayerState
+    if( e.data != e.target.uLastPlayerState ) {
+        // console.log(label + ":state change from " + e.target.uLastPlayerState + " to " + e.data);
+        e.target.uLastPlayerState = e.data;
     }
 }
-function stopVideo() {
-    player1.stopVideo();
+function initYoutubePlayers(){
+    ytplayerList = null; //reset
+    ytplayerList = []; //create new array to hold youtube player
+    for (var e = document.getElementsByTagName("iframe"), x = e.length; x-- ;) {
+        if (/youtube.com\/embed/.test(e[x].src)) {
+            ytplayerList.push(initYoutubePlayer(e[x]));
+            // console.log("create a Youtube player successfully");
+        }
+    }
+    
 }
+function pauseOthersYoutubes( currentPlayer ) {
+    if (!currentPlayer) return;
+    for (var i = ytplayerList.length; i-- ;){
+        if( ytplayerList[i] && (ytplayerList[i] != currentPlayer) ){
+            ytplayerList[i].pauseVideo();
+        }
+    }  
+}
+//init a youtube iframe
+function initYoutubePlayer(ytiframe){
+    // console.log("have youtube iframe");
+    var ytp = new YT.Player(ytiframe, {
+        events: {
+            onStateChange: onPlayerStateChange,
+            onError: onPlayerError,
+            onReady: onPlayerReady
+        }
+    });
+    ytiframe.ytp = ytp;
+    return ytp;
+}
+function onYouTubeIframeAPIReady() {
+    // console.log("YouTubeIframeAPI is ready");
+    initYoutubePlayers();
+}
+var tag = document.createElement('script');
+//use https when loading script and youtube iframe src since if user is logging in youtube the youtube src will switch to https.
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);  
