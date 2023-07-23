f0981a1ebab70447a962c253efa013806e5c140f
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-07-22 19:38:39 -0700

    me border update

---
+++ meBorder.js
@@ -0,0 +1,8 @@
+function changeBorderToYellow() {
+    const image = document.getElementById("me");
+    image.style.border = "8.5px ridge #FFFDD0";
+}
+
+window.addEventListener("load", function () {
+    changeBorderToYellow();
+});
