88b7aadd7654f09a95481b9c3fd3beb0e65ed5ed
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-11-23 22:50:36 -0800

    flip vid

---
+++ flipVideo.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const flipButton = document.getElementById('flipButton');
+    const video = document.querySelector('.video');
+
+    flipButton.addEventListener('click', () => {
+        video.classList.toggle('flipped');
+    });
+});
+
