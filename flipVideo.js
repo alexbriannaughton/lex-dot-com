97cfdc66da0c0227baa2aa8b0ed6c970148aee4b
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-12-05 09:23:40 -0800

    update

--- flipVideo.js
+++ flipVideo.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const flipButton = document.getElementById('flipButton');
-    const video = document.querySelector('.video');
+    const video = document.getElementById('video');
 
     flipButton.addEventListener('click', () => {
         video.classList.toggle('flipped');
